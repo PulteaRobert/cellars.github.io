0c308628318796604058cfe643c1802c35e9e4fd
octa42 <octavicristea@gmail.com>
2019-08-05 12:36:50 +0300

    dfsfd

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mainScreen />
   </div>
 </template>
 
 <script>
+
 import HelloWorld from './components/HelloWorld.vue';
+import mainScreen from './components/mainScreen.vue';
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    mainScreen
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
