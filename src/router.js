10da71213d6f6f2f4123943e3dab17bee403c2d0
Nutzu <glizard.glizard@yahoo.com>
2019-08-07 12:47:39 +0300

    Added credits

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 
 import Game from './routes/Game.vue';
 import Menu from './routes/Menu.vue';
+import Credits from './routes/Credits.vue';
 
 Vue.use(VueRouter);
 
@@ -10,5 +11,6 @@ export default new VueRouter({
   routes: [
     { path: '/', name: 'menu', component: Menu },
     { path: '/game', name: 'game', component: Game },
+    { path: '/credits', name: 'credits', component: Credits },
   ],
 });
