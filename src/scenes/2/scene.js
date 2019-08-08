093fab5627980863a44016061b108c09beb27a57
mihnea <mihn.stn@gmail.com>
2019-08-08 14:25:07 +0300

    new assets

---
+++ src/scenes/2/scene.js
@@ -0,0 +1,17 @@
+import utils from '@/utils';
+
+import bgWayToLab from '@/assets/wayToLab.jpg';
+
+import charMrEnglsHurt from '@/assets/wounded.engls.png';
+import charSalamandr from '@/assets/pistolar.salmendr.png';
+
+export default [
+  {
+    image: bgWayToLab,
+    speaker: '',
+    dialogue: '',
+    color: utils.rgb(166, 166, 166),
+    squad: [charMrEnglsHurt],
+    encounters: [charSalamandr],
+  },
+];
