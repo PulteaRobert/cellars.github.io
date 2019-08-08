c5b0042bda1a4f8ca0bdfefed856f2214f577fe0
mihnea <mihn.stn@gmail.com>
2019-08-08 11:29:34 +0300

    we begin here <<<

--- src/data.js
+++ src/data.js
@@ -1,22 +1,9 @@
-import mrEngls from './assets/MrEngls.png';
-import bg from './assets/ForestPixelPurple.jpg';
-import pistolar from './assets/pistolar.png';
-
-import sax from './assets/sound/test.mp3';
-import nush from './assets/sound/test2.mp3';
+import forest from './assets/forest.jpg';
 
 export default [
   {
-    image: bg,
-  },
-  {
-    squad: [mrEngls],
-    speaker: 'Mr Engls',
-    dialogue: 'no god, pliz no!',
-  },
-  {
-    encounters: [pistolar],
-    speaker: 'pistolar',
-    dialogue: 'heeh',
+    image: forest,
+    color: 'black',
+    sfx: 'stop',
   },
 ];
