a04a5ccd1919ec0836779ca979317aa002cdca32
Nutzu <glizard.glizard@yahoo.com>
2019-08-08 11:37:35 +0300

    na ma poulea vomit

--- src/data.js
+++ src/data.js
@@ -1,9 +1,28 @@
-import forest from './assets/forest.jpg';
+import mrEngls from './assets/MrEngls.png';
+import bg from './assets/scene1.gif';
+import pistolar from './assets/pistolar.png';
+import sax from './assets/sound/SaxSeal.mp3';
 
 export default [
   {
-    image: forest,
-    color: 'black',
+    image: bg,
+    color: 'rgb(138, 0, 236)',
+  },
+  {
+    squad: [mrEngls],
+    speaker: 'Mr Engls',
+    dialogue: 'No god, pliz no!',
+    sfx: sax,
+  },
+  {
+    encounters: [pistolar],
+    speaker: 'Pistolar',
+    dialogue: 'heeh',
     sfx: 'stop',
+  },
+  {
+    choices:[
+      
+    ]
   },
 ];
