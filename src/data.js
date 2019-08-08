1f5d6e51db3bcd9ae6f008601feb492550977e31
AlexandruBularca <alexandru.bularca.bv@gmail.com>
2019-08-08 12:48:46 +0300

    protecc

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ import mrEngls from './assets/MrEngls.png';
 import bg from './assets/scene1.gif';
 import pistolar from './assets/pistolar.png';
 import sax from './assets/sound/SaxSeal.mp3';
+import salvede from './assets/SalSeesYou.png';
 
 import utils from './utils';
 
@@ -30,22 +31,55 @@ export default [
           'run',
           {
             flag: 'success',
-            chance: 0.5,
+            chance: 0.4,
           },
         ],
       },
       {
         text: 'protecc Mr. Engls',
-        flag: 'protecc',
+        flags: 'protecc',
       },
       {
         text: 'distract with a noise',
-        flag: 'distract',
+        flags: 'distract',
       },
     ],
   },
   {
-    if: ['run'],
-
+    choices: [
+      {
+        text: 'grab a sharp stone and aim at the head',
+        flags: [
+          'protecc1',
+          {
+            flag: 'protecc1-succes',
+            chance: 0.5,
+          },
+        ],
+        flags: [
+          'protecc1',
+          {
+            flag: 'protecc1-fail',
+            chance: 0.5,
+          },
+        ],
+      },
+      {
+        text: 'grab a brush and put a little makeup',
+        flags: 'protecc2',
+      },
+      {
+        text: 'stop right there you criminal scum!',
+        flags: 'protecc3',
+      },
+      {
+        text: 'attacc',
+        flags: 'protecc4',
+      },
+    ],
+  },
+  {
+    if: ['protecc1', 'protecc1-succes'],
+    encounters: [salvede],
   },
 ];
