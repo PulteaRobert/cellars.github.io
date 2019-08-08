1c8707b0e925235270d96ea8b4542de7697bff60
octa42 <octavicristea@gmail.com>
2019-08-08 15:10:05 +0300

    distract

--- src/scenes/0/choices/distract.js
+++ src/scenes/0/choices/distract.js
@@ -1,4 +1,8 @@
 import sadviolin from '@/assets/sound/sadv.mp3';
+import tgftheme from '@/assets/sound/tgftheme.mp3';
+import pinacollada from '@/assets/sound/pinacollada.mp3';
+import wiz from '@/assets/sound/wutizluv.mp3';
+import salamandrlooking from '@/assets/SalSeesYou.png';
 
 export default [
   {
@@ -8,6 +12,7 @@ export default [
     sfx: sadviolin,
   },
   {
+    if: ['distract'],
     sfx: 'stop',
     speaker: 'You',
     choices: [
@@ -28,6 +33,34 @@ export default [
       {
         text: 'what is love?',
         flags: 'wil?',
+      },
+    ],
+  },
+  {
+    if: ['offer-success'],
+    choices: [],
+    sfx: tgftheme,
+    encounters: [salamandrlooking],
+  },
+  {
+    if: ['collabs'],
+    choices: [],
+    sfx: pinacollada,
+    encounters: [salamandrlooking],
+  },
+  {
+    if: ['wil?'],
+    choices: [],
+    sfx: wiz,
+    encounters: [salamandrlooking],
+  },
+  {
+    dialogue: 'fndfjdsljfdsj',
+    flags: [
+      'offer',
+      {
+        flag: 'offer-success',
+        chance: 0.5,
       },
     ],
   },
