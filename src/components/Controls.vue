94de86416496d637bc51375d02ade546313a1baa
mihnea <mihn.stn@gmail.com>
2019-08-09 10:35:30 +0300

    E V E N M O R E R E S P O N S I V E D E S I G N

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -84,7 +84,9 @@ export default {
 
 .next-btn:active,
 .next-btn:focus {
-  transform: translate(1vh, 1vh);
+  position: relative;
+  margin-left: 1vh;
+  bottom: -1vh;
   text-shadow: none;
 }
 
@@ -134,9 +136,14 @@ export default {
 @media (min-aspect-ratio: 6/7) and (max-aspect-ratio: 11/7) {
   .controls {
     height: 45vh;
+    flex-direction: row;
   }
   .dialogue {
     flex-grow: 2;
+  }
+
+  .choices {
+    margin-top: 0;
   }
 }
 </style>
