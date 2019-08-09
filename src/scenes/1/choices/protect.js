9910b255b11587d2e60a6f7a4a598fc206746196
mihnea <mihn.stn@gmail.com>
2019-08-09 11:01:58 +0300

    prisoner

--- src/scenes/1/choices/protect.js
+++ src/scenes/1/choices/protect.js
@@ -8,25 +8,25 @@ export default [
       {
         text: 'grab a sharp stone and aim at the head',
         flags: [
-          'protecc1',
+          'protect1',
           {
-            flag: 'protecc1-succes',
+            flag: 'protect1-success',
             chance: 0.5,
-          },
-          'protecc23',
-          {
-            flag: 'protecc23-succes',
-            chance: 1,
           },
         ],
       },
-      { text: 'grab a brush and put a little makeup', flags: 'protecc23' },
-      { text: 'stop right there you criminal scum!', flags: 'protecc23' },
+      {
+        text: 'grab a brush and put a little makeup',
+        flags: 'protect23',
+      },
+      {
+        text: 'stop right there you criminal scum!',
+        flags: 'protect23',
+      },
     ],
   },
   {
-    // ** WORK IN PROGRESS DORESCT SALMANDER MORT SI ENGELS FARA MAINI IN AER MARCA UNTOLD
-    if: ['protecc1', 'protecc1-succes'],
+    if: ['protect1', 'protect1-success'],
     encounters: [chr.salamandrDead],
     squad: [chr.engls],
     speaker: 'Narrator',
@@ -35,24 +35,23 @@ export default [
     jumpto: '3',
   },
   {
-    if: ['protecc1', '!protecc1-succes'],
+    if: ['protect1', '!protect1-success'],
     choices: [],
     squad: [chr.englsWounded],
     sfx: sfx.gunshot,
   },
   {
-    if: ['protecc1', '!protecc1-succes'],
+    if: ['protect1', '!protect1-success'],
     sfx: sfx.fool,
     dialogue:
       'you fool. you absolute buffoon. you think you can challenge me in my own realm? you think you can rebel against my authority? you dare come into my house and upturn my dining chairs and spill coffee grounds in my Keurig? you thought you were safe in your chain mail armor behind that screen of yours. I will take these laminate wood floor boards and destroy you. I didn’t want war. but i didn’t start it.',
     jumpto: '2',
   },
-
   {
-    if: ['protecc23', 'protecc23-succes'],
+    if: ['protect23'],
     sfx: sfx.Criminal,
     speaker: 'Narrator',
-    // IMI TREBUIE chr.EnglsInchisoare
+    squad: [chr.englsPrisoner],
     dialogue: 'Your foolish attempt led to one thing... Being imprisoned',
     jumpto: '2',
   },
