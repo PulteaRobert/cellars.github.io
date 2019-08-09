791480601ff55b369ba88a8128712e2d207e9f6d
Robip123 <alexandru.bularca.bv@gmail.com>
2019-08-09 10:42:38 +0300

    ?????

--- src/scenes/1/choices/protect.js
+++ src/scenes/1/choices/protect.js
@@ -16,21 +16,22 @@ export default [
           'protecc23',
           {
             flag: 'protecc23-succes',
-            chance: 0.5,
+            chance: 1,
           },
         ],
       },
-      { text: 'grab a brush and put a little makeup', flags: 'protecc2' },
-      { text: 'stop right there you criminal scum!', flags: 'protecc3' },
-      { text: 'attacc', flags: 'protecc4' },
+      { text: 'grab a brush and put a little makeup', flags: 'protecc23' },
+      { text: 'stop right there you criminal scum!', flags: 'protecc23' },
     ],
   },
-  { // ** WORK IN PROGRESS DORESCT SALMANDER MORT SI ENGELS FARA MAINI IN AER MARCA UNTOLD
+  {
+    // ** WORK IN PROGRESS DORESCT SALMANDER MORT SI ENGELS FARA MAINI IN AER MARCA UNTOLD
     if: ['protecc1', 'protecc1-succes'],
     encounters: [chr.salamandrDead],
     squad: [chr.engls],
     speaker: 'Narrator',
     dialogue: 'You lucky frog....',
+    jumpto: '3',
   },
   {
     if: ['protecc1', '!protecc1-succes'],
@@ -40,11 +41,16 @@ export default [
   {
     if: ['protecc1', '!protecc1-succes'],
     sfx: sfx.fool,
-    dialogue: 'you fool. you absolute buffoon. you think you can challenge me in my own realm? you think you can rebel against my authority? you dare come into my house and upturn my dining chairs and spill coffee grounds in my Keurig? you thought you were safe in your chain mail armor behind that screen of yours. I will take these laminate wood floor boards and destroy you. I didn’t want war. but i didn’t start it.',
+    dialogue:
+      'you fool. you absolute buffoon. you think you can challenge me in my own realm? you think you can rebel against my authority? you dare come into my house and upturn my dining chairs and spill coffee grounds in my Keurig? you thought you were safe in your chain mail armor behind that screen of yours. I will take these laminate wood floor boards and destroy you. I didn’t want war. but i didn’t start it.',
+    jumpto: '2',
   },
   {
     if: ['protecc23', 'protecc23-succes'],
-    sfx: sfx.fool,
-    dialogue: '',
+    sfx: sfx.Criminal,
+    speaker: 'Narrator',
+    // IMI TREBUIE chr.EnglsInchisoare
+    dialogue: 'Your foolish attempt led to one thing... Being imprisoned',
+    jumpto: '2',
   },
 ];
